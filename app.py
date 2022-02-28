4d0d527f5289de0d5cee75ccf0f1743a2aaf7b82
Yash Goyal <yashgoyalcs@gmail.com>
2022-03-01 03:58:30 +0530

    add code

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ def github_webhook():
     owner = output['repository']['owner']['html_url']
     base_branch = output['pull_request']['base']['label']
     head_branch = output['pull_request']['head']['label']
-    head_url = output['pull_request']['head']['url']
+    head_url = output['pull_request']['url']
     sender = output['sender']['html_url']
-    pp(output)
+    # pp(output)
     status_json = requests.get(head_url, headers={"Accept": "application/vnd.github.v3+json"})
     status = status_json.json()
     log = f"Action: {action} - Repository: {repository} - Owner: {owner} - Base_Branch: {base_branch} - Head_Branch: {head_branch} - Sender: {sender} - Merged: {status['merged']} - Mergeable: {status['mergeable']} - Mergeable State: {status['mergeable_state']}"
