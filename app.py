c801db16f0a8deca85269dfcd63d9f0d95448e19
Yash Goyal <yashgoyalcs@gmail.com>
2022-03-01 03:12:07 +0530

    add code

--- app.py
+++ app.py
@@ -14,15 +14,17 @@ def github_webhook():
     output = request.json
     action = output.get('action')
     repository = output['repository']['html_url']
+    repository_name = output['repository']['name']
     owner = output['repository']['owner']['html_url']
+    owner_name = output['repository']['owner']['login']
     base_branch = output['pull_request']['base']['label']
-    base_branch_ref = output['pull_request']['base']['ref']
+    base_branch_ref = output['pull_request']['base']['sha']
     head_branch = output['pull_request']['head']['label']
     sender = output['sender']['html_url']
     pp(output)
     base_branch_data = base_branch.split(":")
-    status_json = requests.get(f"https://api.github.com/repos/{base_branch_data[0]}/{base_branch_data[1]}/commits/{base_branch_ref}/statuses", headers={"Accept": "application/vnd.github.v3+json"})
-    pp(status_json)
+    status_json = requests.get(f"https://api.github.com/repos/{owner_name}/{repository_name}/commits/{base_branch_ref}/statuses", headers={"Accept": "application/vnd.github.v3+json"})
+    pp(status_json.json())
     log = f"Action: {action} - Repository: {repository} - Owner: {owner} - Base_Branch: {base_branch} - Head_Branch: {head_branch} - Sender: {sender}"
     print(log)
     return "Done"
