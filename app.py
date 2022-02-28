21e4afc74c6a62c2e2e939c12df080c880ba56d5
Yash Goyal <yashgoyalcs@gmail.com>
2022-03-01 02:45:41 +0530

    add code

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+from email import header
 from flask import Flask, request
 from pprint import pprint as pp
+import requests
 
 app = Flask(__name__)
 
@@ -14,9 +16,13 @@ def github_webhook():
     repository = output['repository']['html_url']
     owner = output['repository']['owner']['html_url']
     base_branch = output['pull_request']['base']['label']
+    base_branch_ref = output['pull_request']['base']['ref']
     head_branch = output['pull_request']['head']['label']
     sender = output['sender']['html_url']
     pp(output)
+    base_branch_data = base_branch.split(":")
+    status_json = requests.get(f"https://api.github.com/repos/{base_branch_data[0]}/{base_branch_data[1]}/commits/{base_branch_ref}/statuses", headers={"Accept": "application/vnd.github.v3+json"})
+    pp(status_json)
     log = f"Action: {action} - Repository: {repository} - Owner: {owner} - Base_Branch: {base_branch} - Head_Branch: {head_branch} - Sender: {sender}"
     print(log)
     return "Done"
