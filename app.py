c6205ff6e9a8c46dd0a4c122ed99770fe7312f5f
Yash Goyal <67310422+yashgoyal07@users.noreply.github.com>
2022-03-01 03:39:12 +0530

    Commit status (#11)
    * add file
    
    * add file
    
    * add file
    
    * add file
    
    * add file
    
    * add code
    
    * add code
    
    * add code

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+from email import header
 from flask import Flask, request
 from pprint import pprint as pp
+import requests
 
 app = Flask(__name__)
 
@@ -12,10 +14,17 @@ def github_webhook():
     output = request.json
     action = output.get('action')
     repository = output['repository']['html_url']
+    repository_name = output['repository']['name']
     owner = output['repository']['owner']['html_url']
+    owner_name = output['repository']['owner']['login']
     base_branch = output['pull_request']['base']['label']
+    base_branch_ref = output['pull_request']['head']['title']
     head_branch = output['pull_request']['head']['label']
     sender = output['sender']['html_url']
+    pp(output)
+    base_branch_data = base_branch.split(":")
+    status_json = requests.get(f"https://api.github.com/repos/{owner_name}/{repository_name}/commits/{base_branch_ref}/statuses", headers={"Accept": "application/vnd.github.v3+json"})
+    pp(status_json.json())
     log = f"Action: {action} - Repository: {repository} - Owner: {owner} - Base_Branch: {base_branch} - Head_Branch: {head_branch} - Sender: {sender}"
     print(log)
     return "Done"
