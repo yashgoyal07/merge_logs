55dea05b676946008be8b33abcc0d78d74e73925
Yash Goyal <yashgoyalcs@gmail.com>
2022-03-04 01:10:02 +0530

    log annotation also

--- jobs_logs.py
+++ jobs_logs.py
@@ -12,11 +12,26 @@ response_json = response.json()
 
 total_jobs = response_json['total_count']
 
-print(f'\033[1mTotal Jobs\033[0m: {total_jobs}\n')
+bold_start = '\033[1m'
+bold_end = '\033[0m'
+
+print(f'{bold_start}Total Jobs{bold_end}: {total_jobs}\n')
 
 jobs = response_json['jobs']
 
 for job in jobs:
-    print(f"\033[1mLogs of job id: {job['id']} given below\033[0m\n")
+    print(f"{bold_start}Logs of job id: {job['id']} given below:{bold_end}\n")
     for step in job['steps']:
-        print(f"--> \033[1mName\033[0m: {step['name']} - \033[1mStatus\033[0m: {step['status']} - \033[1mConclusion\033[0m: {step['conclusion']}\n")
+        print(f"--> {bold_start}Name{bold_end}: {step['name']} - {bold_start}Status{bold_end}: {step['status']} - {bold_start}Conclusion{bold_end}: {step['conclusion']}\n")
+
+    check_run_url = job['check_run_url']
+
+    annotation_url = check_run_url + '/annotations'
+
+    annotations = requests.get(annotation_url, headers={"Accept": "application/vnd.github.v3+json"})
+
+    annotations_json = annotations.json()
+
+    print(f"{bold_start}-- > Logs of annotations for this job given below:{bold_end}\n")
+    for annotation in annotations_json:
+        print(f"   --> {bold_start}Level{bold_end}: {annotation['annotation_level']} - {bold_start}Title{bold_end}: {annotation['title']} - {bold_start}Message{bold_end}: {annotation['message']}\n")
